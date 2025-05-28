602510ac65973169f7bf4964a179e8abf09d9a25
Johnathan MEUNIER <johnathan2693@gmail.com>
2025-05-28 15:51:51 +0200

    feat(Todo): ajout de todo

---
+++ front/src/components/Add/Add.tsx
@@ -0,0 +1,40 @@
+import { type Dispatch, type FormEvent, type SetStateAction } from "react";
+
+import { type todo } from "../../types";
+
+const Add = ({ setTodos }: { setTodos: Dispatch<SetStateAction<todo[]>> }) => {
+  const handleSubmit = (e: FormEvent<HTMLFormElement>) => {
+    e.preventDefault();
+    const form = new FormData(e.currentTarget);
+
+    console.log(form.get("deadline"));
+
+    setTodos(todos => ([
+      ...todos,
+      {
+        key: todos.length + 1,
+        label: form.get("label") as string,
+        tags: (form.get("tags") as string).split(','),
+        deadline: new Date(form.get("deadline") as string),
+        done: false
+      }
+    ]))
+    // e.currentTarget.reset();
+  }
+  return <>
+    <h2>
+      Ajouter
+    </h2>
+    <form onSubmit={handleSubmit}>
+      <label htmlFor="label">Libellé</label>
+      <input type="text" name="label" id="label" placeholder="Libellé de ma tâche" />
+      <label htmlFor="tags">Tags</label>
+      <input type="text" name="tags" id="tags" placeholder="tag1,tag2,tag3" />
+      <label htmlFor="deadline">Echéance</label>
+      <input type="date" name="deadline" id="deadline" />
+      <button type="submit">Ajouter</button>
+    </form>
+  </>
+}
+
+export default Add;
--- front/src/pages/Dashboard/Dashboard.tsx
+++ front/src/pages/Dashboard/Dashboard.tsx
@@ -1,15 +1,19 @@
+import { useEffect, useState } from "react";
 import Todo from "@components/Todo";
+import Add from "@components/Add";
 import { type todo } from "../../types";
 import { data } from "../../data";
 
 const Dashboard = () => {
+  const [todos, setTodos] = useState(data);
+
   return <>
     <h2>Dashboard</h2>
     {
-      data.map(({ key, label, deadline, done, tags }: todo) =>
+      todos.map(({ key, label, deadline, done, tags }: todo) =>
         <Todo key={key} label={label} tags={tags} deadline={deadline} done={done} />)
     }
-
+    <Add setTodos={setTodos} />
   </>
 }
 
