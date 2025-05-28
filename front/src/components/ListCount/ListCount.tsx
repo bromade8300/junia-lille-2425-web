e90bf7ea1884fedc92d9b92cbf7b87f2eb429418
Johnathan MEUNIER <johnathan2693@gmail.com>
2025-05-28 09:45:12 +0200

    first commit

---
+++ front/src/components/ListCount/ListCount.tsx
@@ -0,0 +1,13 @@
+import Count from '@components/Count';
+
+const ListCount = () => <ul>
+  <li>
+    <Count />
+    <Count />
+    <Count />
+    <Count />
+    <Count />
+  </li>
+</ul>
+
+export default ListCount;
---
+++ front/vite.config.ts
@@ -0,0 +1,20 @@
+import { defineConfig, type UserConfig } from 'vite'
+import react from '@vitejs/plugin-react'
+import { fileURLToPath } from 'url'
+import { dirname, resolve } from 'path'
+
+const __filename = fileURLToPath(import.meta.url)
+const __dirname = dirname(__filename)
+
+// https://vite.dev/config/
+export default defineConfig({
+  plugins: [react()],
+  resolve: {
+    alias: {
+      '@layouts': resolve(__dirname, './src/layouts'),
+      '@services': resolve(__dirname, './src/services'),
+      '@components': resolve(__dirname, './src/components'),
+      '@pages': resolve(__dirname, './src/pages'),
+    }
+  },
+})
