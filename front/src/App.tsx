32519584135fe56d7785799543e098b5923c3801
Johnathan MEUNIER <johnathan2693@gmail.com>
2025-05-28 15:16:15 +0200

    feat(style): init style

--- front/src/App.tsx
+++ front/src/App.tsx
@@ -1,15 +1,15 @@
-import { useState } from 'react';
 import Dashboard from '@pages/Dashboard';
-
-import './App.css'
+import style from "./app.module.scss";
 
 function App() {
   return (
-    <>
-      <header><h1>My Todo App</h1></header>
-      <Dashboard />
-      <footer>Junia - WEB - 2425</footer>
-    </>
+    <div className={style.layout}>
+      <header className={style.header}><h1 className={style.title}>My Todo App</h1></header>
+      <main className={style.container}>
+        <Dashboard />
+      </main>
+      <footer className={style.footer}>Junia - WEB - 2425</footer>
+    </div>
   )
 }
 
--- front/src/components/Todo/Todo.tsx
+++ front/src/components/Todo/Todo.tsx
@@ -1,17 +1,22 @@
 import { v4 as uuidv4 } from 'uuid';
 import { type todo } from '../../types';
+import style from "./todo.module.scss";
 
 const Todo = ({ label, tags, deadline, done }: todo) => {
   return (
-    <div className="todo">
-      <h3>{label}</h3>
-      <ul>
-        {tags.map(tag => <li key={uuidv4()}>{tag}</li>)}
-      </ul>
-      <p>{new Intl.DateTimeFormat("fr-FR", {
-        dateStyle: "full"
-      }).format(deadline)}</p>
-      <input type="checkbox" name="done" checked={done} />
+    <div className={style.todo}>
+      <input type="checkbox" name="done" checked={done} className={style.check} />
+      <div className="info">
+        <h3 className={style.title}>{label}</h3>
+        <div className={style.meta}>
+          <ul className={style.tags}>
+            {tags.map(tag => <li key={uuidv4()} className={style.tag}>#{tag}</li>)}
+          </ul>
+          <span className={style.date}>{new Intl.DateTimeFormat("fr-FR", {
+            dateStyle: "full"
+          }).format(deadline)}</span>
+        </div>
+      </div>
     </div>
   )
 }
