05a709da2de9611cb768c9e90fb258ae618574be
Johnathan MEUNIER <johnathan2693@gmail.com>
2025-06-03 16:34:30 +0200

    feat(front): loader

--- front/src/pages/Dashboard/Dashboard.tsx
+++ front/src/pages/Dashboard/Dashboard.tsx
@@ -7,15 +7,18 @@ import { type todo } from "../../types";
 const Dashboard = () => {
   const [todos, setTodos] = useState<todo[]>([]);
   const [error, setError] = useState<null | string>(null);
-
+  const [isLoading, setIsLoading] = useState(false);
   useEffect(() => {
     const fetchData = async () => {
+      setIsLoading(true);
       const res = await fetch("http://localhost:3000/todo");
       console.log(res);
       if (!res.ok) {
+        setIsLoading(false);
         setError("Une erreur est survenue");
       } else {
         const result = await res.json();
+        setIsLoading(false);
         setTodos(result);
       }
     }
@@ -25,21 +28,24 @@ const Dashboard = () => {
   const [filterByNotDone, setFilterByNotDone] = useState(true);
   return <>
     <h2>Dashboard</h2>
-    {error && <p>{error}</p>}
+    {
+      isLoading ? error && <p>{error}</p> : <ul>
+        {
+          todos
+            .filter(({ done }) => {
+              if (filterByNotDone) {
+                return !done
+              }
+              return true
+            })
+            .map(({ id, label, deadline, done, tags }: todo) =>
+              <Todo key={id} id={id} label={label} tags={tags} deadline={deadline} done={done} setTodos={setTodos} />)
+        }
+      </ul>
+    }
+
     <label htmlFor="displayAllTodos">Afficher toutes les todos</label> <input type="checkbox" name="displayAllTodos" id="displayAllTodos" checked={!filterByNotDone} onChange={() => setFilterByNotDone(filterByNotDone => !filterByNotDone)} />
-    <ul>
-      {
-        todos
-          .filter(({ done }) => {
-            if (filterByNotDone) {
-              return !done
-            }
-            return true
-          })
-          .map(({ id, label, deadline, done, tags }: todo) =>
-            <Todo key={id} id={id} label={label} tags={tags} deadline={deadline} done={done} setTodos={setTodos} />)
-      }
-    </ul>
+
     <Add setTodos={setTodos} />
   </>
 }
