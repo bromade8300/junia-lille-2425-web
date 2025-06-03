2a5d6aa113d1df2ee45cade98571520246ecdf82
Johnathan MEUNIER <johnathan2693@gmail.com>
2025-06-03 14:40:57 +0200

    refactor(back): read init data throught a file

--- back/index.js
+++ back/index.js
@@ -1,4 +1,5 @@
 const express = require("express");
+const { readFileSync } = require("fs");
 const port = 3000;
 const app = express();
 
@@ -7,43 +8,8 @@ app.get('/', (req, res) => {
 })
 
 app.get("/todo", (req, res) => {
-  res.json([
-    {
-      id: 1,
-      label: "Finaliser le rapport mensuel",
-      tags: ["travail", "rapport", "urgent"],
-      deadline: new Date("2024-03-25T17:00:00"),
-      done: false
-    },
-    {
-      id: 2,
-      label: "Réservation restaurant anniversaire",
-      tags: ["personnel", "événement"],
-      deadline: new Date("2024-03-30T12:00:00"),
-      done: false
-    },
-    {
-      id: 3,
-      label: "Mise à jour des dépendances npm",
-      tags: ["développement", "maintenance"],
-      deadline: new Date("2024-03-22T16:00:00"),
-      done: true
-    },
-    {
-      id: 4,
-      label: "Préparation entretien d'embauche",
-      tags: ["carrière", "préparation"],
-      deadline: new Date("2024-03-28T10:00:00"),
-      done: false
-    },
-    {
-      id: 5,
-      label: "Révision du code pour la release",
-      tags: ["développement", "code review"],
-      deadline: new Date("2024-03-27T14:00:00"),
-      done: false
-    }
-  ])
+  const data = readFileSync("./data.json", { encoding: 'utf8', flag: 'r' });
+  res.json(JSON.parse(data));
 });
 
 app.listen(port, () => {
