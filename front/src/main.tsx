15a8dbb8c8811cb7e3ac31dc90f25ab4cedfacdf
Johnathan MEUNIER <johnathan2693@gmail.com>
2025-06-18 10:03:27 +0200

    chore(msw): initial setup

--- front/src/main.tsx
+++ front/src/main.tsx
@@ -5,17 +5,27 @@ import {
   QueryClientProvider,
 } from '@tanstack/react-query'
 import { ReactQueryDevtools } from '@tanstack/react-query-devtools'
-
-// Create a client
-const queryClient = new QueryClient()
 import './index.css'
 import App from './App.tsx'
 
-createRoot(document.getElementById('root')!).render(
-  <BrowserRouter>
-    <QueryClientProvider client={queryClient}>
-      <ReactQueryDevtools initialIsOpen={false} />
-      <App />
-    </QueryClientProvider>
-  </BrowserRouter>
-)
+// Create a client
+const queryClient = new QueryClient()
+
+async function enableMocking() {
+  const { worker } = await import('./mocks/browser')
+
+  // `worker.start()` returns a Promise that resolves
+  // once the Service Worker is up and ready to intercept requests.
+  return worker.start()
+}
+
+enableMocking().then(() => {
+  createRoot(document.getElementById('root')!).render(
+    <BrowserRouter>
+      <QueryClientProvider client={queryClient}>
+        <ReactQueryDevtools initialIsOpen={false} />
+        <App />
+      </QueryClientProvider>
+    </BrowserRouter>
+  )
+})
